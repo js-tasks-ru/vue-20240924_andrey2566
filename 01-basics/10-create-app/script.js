daa4630cb8694ce75733d2f75e67997c3feb1435
zizaraDzr <elikdzr@mail.ru>
2024-09-30 10:30:18 +0300

    resolved 01-10 task createApp

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,15 @@
 import { defineComponent, createApp } from 'vue'
+
+
+const App = defineComponent({
+  name: 'App',
+  methods: {
+    formatAsLocalDate () {
+      return new Date().toLocaleDateString(navigator.language, { dateStyle: 'long' })
+    }
+  },
+
+  template: `<div>Сегодня {{formatAsLocalDate()}}</div>`,
+})
+
+createApp(App).mount('#app')
