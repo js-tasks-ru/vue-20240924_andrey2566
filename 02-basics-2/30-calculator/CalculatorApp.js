79fd59bbe3ca8f1f1613d38f6da5bc1819ca1879
zizaraDzr <elikdzr@mail.ru>
2024-10-03 11:13:52 +0300

    task 2-30-calculator

--- 02-basics-2/30-calculator/CalculatorApp.js
+++ 02-basics-2/30-calculator/CalculatorApp.js
@@ -1,26 +1,46 @@
-import { defineComponent } from 'vue'
+import { computed, defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'CalculatorApp',
 
-  setup() {},
+  setup() {
+    const mathOperations = {
+      sum: function (x, y) { return x + y },
+      subtract: function (x, y) { return x - y },
+      multiply: function (x, y) { return x * y },
+      divide: function (x, y) { return x / y },
+    }
+    const operand = ref({ first: 0, second: 0, })
+    const operator = ref('')
+
+    const sum = computed(() => {
+      if (!operator.value) return 0
+      return mathOperations[operator.value](operand.value.first, operand.value.second)
+    })
+
+    return {
+      operand,
+      operator,
+      sum,
+    }
+  },
 
   template: `
     <div class="calculator">
-      <input type="number" aria-label="First operand" />
+      <input type="number" aria-label="First operand" v-model="operand.first" />
 
       <div class="calculator__operators">
-        <label><input type="radio" name="operator" value="sum"/>➕</label>
-        <label><input type="radio" name="operator" value="subtract"/>➖</label>
-        <label><input type="radio" name="operator" value="multiply"/>✖</label>
-        <label><input type="radio" name="operator" value="divide"/>➗</label>
+        <label><input type="radio" name="operator" value="sum" v-model="operator"/>➕</label>
+        <label><input type="radio" name="operator" value="subtract"  v-model="operator"/>➖</label>
+        <label><input type="radio" name="operator" value="multiply"  v-model="operator"/>✖</label>
+        <label><input type="radio" name="operator" value="divide"  v-model="operator"/>➗</label>
       </div>
 
-      <input type="number" aria-label="Second operand" />
+      <input type="number" aria-label="Second operand" v-model="operand.second"/>
 
       <div>=</div>
 
-      <output>0</output>
+      <output>{{ sum }}</output>
     </div>
   `,
 })
