9976d45c6ca789b4f81e11e2b09d0c14656a248c
zizaraDzr <elikdzr@mail.ru>
2024-10-07 10:17:09 +0300

    task 03-50-weather-components
    Поправил 03-40-UiCounter

--- 03-components/50-weather-components/WeatherApp.js
+++ 03-components/50-weather-components/WeatherApp.js
@@ -1,52 +1,57 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref, computed } from 'vue'
 import { getWeatherData, WeatherConditionIcons } from './weather.service.ts'
+import WeatherCard from './WeatherCard.js'
 import './WeatherApp.css'
 
 export default defineComponent({
   name: 'WeatherApp',
+  components: {
+    WeatherCard,
+  },
+
+  setup() {
+    const weather = ref(getWeatherData())
+    const weatherConditionIcons = WeatherConditionIcons
+
+    // Функция для получения иконки погоды по id
+    const getIcon = id => {
+      return weatherConditionIcons[id]
+    }
+
+    const calculationWeather = computed(() => {
+      return weather.value.map(item => {
+        if (item.current.temp) {
+          let temp = (item.current.temp - 273.15).toFixed(1)
+          item.current.temp = `${temp} °C`
+        }
+        item.current.calcIcon = getIcon(item.current.weather.id)
+        if (item.current.pressure) {
+          item.current.pressure = (item.current.pressure * 0.75).toFixed(0)
+        }
+        if (item.current.dt) {
+          item.current.isNight = item.current.dt < item.current.sunrise && item.current.dt < item.current.sunset
+        }
+
+        return item
+      })
+    })
+
+    return {
+      calculationWeather,
+    }
+  },
 
   template: `
     <div>
       <h1 class="title">Погода в Средиземье</h1>
 
       <ul class="weather-list unstyled-list">
-        <li class="weather-card weather-card--night">
-          <div class="weather-alert">
-            <span class="weather-alert__icon">⚠️</span>
-            <span class="weather-alert__description">Королевская метеослужба короля Арагорна II: Предвещается наступление сильного шторма.</span>
-          </div>
-          <div>
-            <h2 class="weather-card__name">
-              Гондор
-            </h2>
-            <div class="weather-card__time">
-              07:17
-            </div>
-          </div>
-          <div class="weather-conditions">
-            <div class="weather-conditions__icon" title="thunderstorm with heavy rain">⛈️</div>
-            <div class="weather-conditions__temp">15.0 °C</div>
-          </div>
-          <div class="weather-details">
-            <div class="weather-details__item">
-              <div class="weather-details__item-label">Давление, мм рт. ст.</div>
-              <div class="weather-details__item-value">754</div>
-            </div>
-            <div class="weather-details__item">
-              <div class="weather-details__item-label">Влажность, %</div>
-              <div class="weather-details__item-value">90</div>
-            </div>
-            <div class="weather-details__item">
-              <div class="weather-details__item-label">Облачность, %</div>
-              <div class="weather-details__item-value">100</div>
-            </div>
-            <div class="weather-details__item">
-              <div class="weather-details__item-label">Ветер, м/с</div>
-              <div class="weather-details__item-value">10.5</div>
-            </div>
-          </div>
+        <li v-for="weather in calculationWeather" class="weather-card"
+          :class="{ 'weather-card--night': weather.current.isNight }">
+          <WeatherCard :weather="weather"/>
         </li>
       </ul>
+
     </div>
   `,
 })
